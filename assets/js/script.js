ea27e641fd9a472c93667c6d0950262c4242c7da
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-12 09:10:30 +0530

    js logic changed switching to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
-let loader=document.querySelectorAll('.loader');
+let loader=document.querySelectorAll('.loader-page'),
+    main_page=document.querySelectorAll('.main-page');
 
 function load(){
 loader[0].style.opacity="0";
+main_page[0].style.filter="blur(0px)";
 }
 
 setInterval(load,2000);
