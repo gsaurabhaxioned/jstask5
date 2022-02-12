84ad635b3bba43fee7fcb144019e8f380fc0ec1a
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-12 10:39:41 +0530

    formatting of js code done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,16 @@
-let loader=document.querySelectorAll('.loader-page'),
-    main_page=document.querySelectorAll('.main-page');
-count=1;
+let loader = document.querySelectorAll('.loader-page'),
+    main_page = document.querySelectorAll('.main-page');
+count = 1;
 
-function counter(){
+function counter() {
     count++;
-    loader[0].innerHTML=count+"%";
-}
-function load(){
- loader[0].style.opacity="0";
-main_page[0].style.filter="blur(0px)";
+    loader[0].innerHTML = count + "%";
 }
 
-setInterval(counter,20);
-setInterval(load,2000);
+function load() {
+    loader[0].style.opacity = "0";
+    main_page[0].style.filter = "blur(0px)";
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+setInterval(counter, 20);
+setInterval(load, 2000);
